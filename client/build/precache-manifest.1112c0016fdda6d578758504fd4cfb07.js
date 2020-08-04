b4cade6bd8eae40a9801258df4a30ebcf9cd19c8
nikssan123 <nikssan123@gmail.com>
2020-08-05 01:45:04 +0300

    adding react build folder

---
+++ client/build/precache-manifest.1112c0016fdda6d578758504fd4cfb07.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03ab8aeb33ab34613fd09f0db8901326",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8021aa2f5b2e9e145384",
+    "url": "/static/css/2.fa3cdfae.chunk.css"
+  },
+  {
+    "revision": "d9643572b3164f14b897",
+    "url": "/static/css/main.5c2876b5.chunk.css"
+  },
+  {
+    "revision": "8021aa2f5b2e9e145384",
+    "url": "/static/js/2.f42f471b.chunk.js"
+  },
+  {
+    "revision": "cd596143dc2b5dcfd815ffd22adc4d40",
+    "url": "/static/js/2.f42f471b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9643572b3164f14b897",
+    "url": "/static/js/main.22551abb.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "43b32b1f349b290d408ae00e183879e9",
+    "url": "/static/media/signed-out-home.43b32b1f.jpg"
+  },
+  {
+    "revision": "25398cf74efabfc8f446dfcb6c2a6338",
+    "url": "/static/media/warbler-logo.25398cf7.png"
+  }
+]);
